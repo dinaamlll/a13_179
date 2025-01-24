0890f16d574e7a54580e7c819e538e24ca4bf773
dinaamlll <dinamaliaa17@gmail.com>
2025-01-25 00:25:14 +0700

    Menambahkan FormInputPeserta untuk input data peserta

--- app/src/main/java/com/example/a13_179/ui/view/peserta/InsertPesertaView.kt
+++ app/src/main/java/com/example/a13_179/ui/view/peserta/InsertPesertaView.kt
@@ -1,19 +1,31 @@
 package com.example.a13_179.ui.view.peserta
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.a13_179.ui.customwidget.CostumeTopAppBar
 import com.example.a13_179.ui.navigation.DestinasiNavigasi
+import com.example.a13_179.ui.viewmodel.event.PenyediaViewModel
+import com.example.a13_179.ui.viewmodel.peserta.InsertPesertaUiEvent
+import com.example.a13_179.ui.viewmodel.peserta.InsertPesertaUiState
+import com.example.a13_179.ui.viewmodel.peserta.InsertPesertaViewModel
 import kotlinx.coroutines.launch
 
 object DestinasiEntryPeserta: DestinasiNavigasi {
@@ -43,8 +55,8 @@ fun EntryPesertaScreen(
         }
     ) { innerPadding ->
         EntryBodyPeserta( //EntryBody untuk form input data peserta dengan tombol simpan.
-            insertPesertaUiState = viewModel.uiState,
-            onPesertaValueChange = viewModel::updateInserPsrtaState,
+            insertPesertaUiState = viewModel.PesertauiState,
+            onPesertaValueChange = viewModel::updateInsertPsrtaState,
             onSaveClick = {
                 coroutineScope.launch {
                     viewModel.insertPsrta()
@@ -55,6 +67,95 @@ fun EntryPesertaScreen(
                 .padding(innerPadding)
                 .verticalScroll(rememberScrollState())
                 .fillMaxWidth()
+        )
+    }
+}
+@Composable
+fun EntryBodyPeserta(
+    insertPesertaUiState: InsertPesertaUiState,
+    onPesertaValueChange: (InsertPesertaUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputPeserta(
+            insertPesertaUiEvent = insertPesertaUiState.insertPesertaUiEvent,
+            onValueChange = onPesertaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputPeserta(
+    insertPesertaUiEvent: InsertPesertaUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertPesertaUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertPesertaUiEvent.nama_peserta,
+            onValueChange = { onValueChange(insertPesertaUiEvent.copy(nama_peserta = it)) },
+            label = { Text("Nama") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = insertPesertaUiEvent.id_peserta?.toString() ?: "",
+            onValueChange = {
+                onValueChange(insertPesertaUiEvent.copy(id_peserta = it.toIntOrNull()))
+            },
+            label = { Text("Id Peserta") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = insertPesertaUiEvent.email,
+            onValueChange = { onValueChange(insertPesertaUiEvent.copy(email = it)) },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = insertPesertaUiEvent.nomor_telepon,
+            onValueChange = { onValueChange(insertPesertaUiEvent.copy(nomor_telepon = it)) },
+            label = { Text("Nomor Telepon") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        if (enabled) {
+            Text(
+                text = "Isi Semua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
         )
     }
 }
