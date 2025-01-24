30232e9ce04b5a8b99837d75c26dd27c53bc9d1e
dinaamlll <dinamaliaa17@gmail.com>
2025-01-24 23:36:20 +0700

    Menambahkan EntryPesertaScreen untuk input data peserta dengan form dan tombol simpan.

---
+++ app/src/main/java/com/example/a13_179/ui/view/peserta/InsertPesertaView.kt
@@ -0,0 +1,60 @@
+package com.example.a13_179.ui.view.peserta
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a13_179.ui.customwidget.CostumeTopAppBar
+import com.example.a13_179.ui.navigation.DestinasiNavigasi
+import kotlinx.coroutines.launch
+
+object DestinasiEntryPeserta: DestinasiNavigasi {
+    override val route = "item_entry_peserta"
+    override val titleRes = "Entry Peserta"
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryPesertaScreen(
+    navigateBack:()->Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertPesertaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntryPeserta.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBodyPeserta(
+            insertPesertaUiState = viewModel.uiState,
+            onMahasiswaValueChange = viewModel::updateInserPsrtaState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertPsrta()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
