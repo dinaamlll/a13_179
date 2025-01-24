b75c94500f4f37eeecd2f599ec18f4eb816f7c47
dinaamlll <dinamaliaa17@gmail.com>
2025-01-24 23:08:25 +0700

    Menambahkan HomeStatus untuk menampilkan loading, data peserta, atau error

--- app/src/main/java/com/example/a13_179/ui/view/peserta/HomePesertaView.kt
+++ app/src/main/java/com/example/a13_179/ui/view/peserta/HomePesertaView.kt
@@ -1,5 +1,7 @@
 package com.example.a13_179.ui.view.peserta
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -10,12 +12,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a13_179.model.Peserta
 import com.example.a13_179.ui.customwidget.CostumeTopAppBar
 import com.example.a13_179.ui.navigation.DestinasiNavigasi
+import com.example.a13_179.ui.view.event.OnError
+import com.example.a13_179.ui.view.event.OnLoading
 import com.example.a13_179.ui.viewmodel.event.PenyediaViewModel
 import com.example.a13_179.ui.viewmodel.peserta.HomePesertaViewModel
 
@@ -30,7 +36,7 @@ object DestinasiHomePeserta: DestinasiNavigasi {
 fun HomePesertaScreen(
     navigateToPesertaEntry:()->Unit,
     modifier: Modifier=Modifier,
-    onDetailClick: (String) -> Unit ={},
+    onDetailClickPeserta: (String) -> Unit ={},
     viewModel: HomePesertaViewModel = viewModel(factory = PenyediaViewModel.Factory)
 
 ){
@@ -58,13 +64,44 @@ fun HomePesertaScreen(
         },
     ) { innerPadding->
         HomeStatus(
-            homeUiState = viewModel.PesertaUIState,
+            homePesertaUiState = viewModel.PesertaUIState,
             retryAction = {viewModel.getPsrta()}, modifier = Modifier.padding(innerPadding),
-            onDetailClick = onDetailClick,onDeleteClick = {
+            onDetailClickPeserta = onDetailClickPeserta,onDeleteClickPeserta = {
                 viewModel.deletePsrta(it.nim)
                 viewModel.getPsrta()
             }
         )
     }
 }
+@Composable
+fun HomeStatus(
+    homePesertaUiState: HomePesertaUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClickPeserta: (Peserta) -> Unit = {},
+    onDetailClickPeserta: (String) -> Unit
+){
+    when (homePesertaUiState){
+        is HomePesertaUiState.Loading-> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomePesertaUiState.Success ->
+            if(homeUiState.peserta.isEmpty()){
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                    Text(text = "Tidak ada data kontak")
+                }
+            }else{
+                PesertaLayout(
+                    peserta = homePesertaUiState.peserta,modifier = modifier.fillMaxWidth(),
+                    onDetailClickPeserta = {
+                        onDetailClickPeserta(it.id_peserta)
+                    },
+                    onDeleteClickPeserta={
+                        onDeleteClickPeserta(it)
+                    }
+                )
+            }
+        is HomePesertaUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
 
