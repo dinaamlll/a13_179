2e9bfdc73ac0183565dff9abcd09f40d613bc856
dinaamlll <dinamaliaa17@gmail.com>
2025-01-24 23:19:31 +0700

    Menambahkan PesertaLayout dan PesertaCard

--- app/src/main/java/com/example/a13_179/ui/view/peserta/HomePesertaView.kt
+++ app/src/main/java/com/example/a13_179/ui/view/peserta/HomePesertaView.kt
@@ -1,19 +1,28 @@
 package com.example.a13_179.ui.view.peserta
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -146,4 +155,82 @@ fun OnError(retryAction:()->Unit, modifier: Modifier = Modifier){
     }
 }
 
+@Composable
+fun PesertaLayout( //PesertaLayout utk menampilkan daftar peserta dengan komponen LazyColumn.
+    peserta: List<Peserta>,
+    modifier: Modifier = Modifier,
+    onDetailClickPeserta:(Peserta)->Unit,
+    onDetailClickPeserta: (Peserta) -> Unit = {}
+){
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(peserta){ peserta ->
+            PesertaCard(
+                peserta = peserta,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable{onDetailClickPeserta(peserta)},
+                onDetailClickPeserta ={
+                    onDetailClickPeserta(peserta)
+                }
+            )
 
+        }
+    }
+}
+
+@Composable
+fun PesertaCard( //PesertaCard utk menampilkan detail peserta dengan opsi hapus
+    peserta: Peserta,
+    modifier: Modifier = Modifier,
+    onDeleteClickPeserta:(Peserta)->Unit={}
+){
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = peserta.nama_peserta,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = { onDeleteClickPeserta(peserta) }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null,
+                    )
+                }
+
+                Text(
+                    text = peserta.id_peserta,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+
+            Text(
+                text = peserta.nama_peserta,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = peserta.email,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = peserta.nomor_telepon,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
