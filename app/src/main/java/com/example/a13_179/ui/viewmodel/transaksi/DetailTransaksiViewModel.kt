31aca85d2bd0493850f361fab42f8c38cc96dd4a
dinaamlll <dinamaliaa17@gmail.com>
2025-01-28 21:15:19 +0700

    menghapus beberapa code yang tidak terpakai dan menambahkan data class DetailTransaksiUiState

--- app/src/main/java/com/example/a13_179/ui/viewmodel/transaksi/DetailTransaksiViewModel.kt
+++ app/src/main/java/com/example/a13_179/ui/viewmodel/transaksi/DetailTransaksiViewModel.kt
@@ -1,71 +1,73 @@
 package com.example.a13_179.ui.viewmodel.transaksi
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.a13_179.model.Peserta
 import com.example.a13_179.model.Tiket
 import com.example.a13_179.model.Transaksi
+import com.example.a13_179.repository.PesertaRepository
 import com.example.a13_179.repository.TiketRepository
 import com.example.a13_179.repository.TransaksiRepository
+import com.example.a13_179.ui.view.peserta.DestinasiDetailPeserta
 import com.example.a13_179.ui.view.tiket.DestinasiDetailTiket
 import com.example.a13_179.ui.view.transaksi.DestinasiDetailTransaksi
+import com.example.a13_179.ui.viewmodel.event.DetailEventUiState
+import com.example.a13_179.ui.viewmodel.event.InsertEventUiEvent
+import com.example.a13_179.ui.viewmodel.event.toDetailEventUiEvent
 import com.example.a13_179.ui.viewmodel.tiket.DetailTiketUiState
+import com.example.a13_179.ui.viewmodel.tiket.InsertTiketUiEvent
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
-sealed class DetailTransaksiUiState {
-    data class Success(val transaski: Transaksi) : DetailTransaksiUiState()
-    object Error : DetailTransaksiUiState()
-    object Loading : DetailTransaksiUiState()
-}
 
 class DetailTransaksiViewModel(
     savedStateHandle: SavedStateHandle,
-    private val trnsksi: TransaksiRepository
+    private val transaksiRepository : TransaksiRepository
 ) : ViewModel() {
 
-    private val _idTransaksi: Int = savedStateHandle.get<String>(DestinasiDetailTransaksi.ID_TRANSAKSI)
-        ?.toIntOrNull()
-        ?: throw IllegalArgumentException("Invalid ID_PESERTA value")
-    // StateFlow untuk menyimpan status UI
-    private val _detailTransaksiUiState = MutableStateFlow<DetailTransaksiUiState>(DetailTransaksiUiState.Loading)
-    val detailTransaksiUiState: StateFlow<DetailTransaksiUiState> = _detailTransaksiUiState
+    var uiState by mutableStateOf(DetailTransaksiUiState())
+        private set
 
-    init {
-        getDetailTransaksi()
-    }
 
-    fun getDetailTransaksi() {
+    fun getDetailTransaksi(id_transaksi: Int) {
         viewModelScope.launch {
+            uiState = DetailTransaksiUiState(isLoading = true)
             try {
-                // Set loading state
-                _detailTransaksiUiState.value = DetailTransaksiUiState.Loading
+                val transaksi = transaksiRepository.getTransaksiById(id_transaksi)
 
-                // Fetch mahasiswa data dari repository
-                val transaksi = trnsksi.getTransaksiById(_idTransaksi)
-
-                if (transaksi != null) {
-                    // Jika data ditemukan, emit sukses
-                    _detailTransaksiUiState.value = DetailTransaksiUiState.Success(transaksi)
-                } else {
-                    // Jika data tidak ditemukan, emit error
-                    _detailTransaksiUiState.value = DetailTransaksiUiState.Error
-                }
+                // Mengubah state dengan data event yang diterima
+                uiState = DetailTransaksiUiState(detailTransaksiUiEvent = transaksi.toDetailTransaksiUiEvent())
             } catch (e: Exception) {
-                // Emit error jika terjadi exception
-                _detailTransaksiUiState.value = DetailTransaksiUiState.Error
+                e.printStackTrace()
+
+                // Menangani error jika request gagal
+                uiState = DetailTransaksiUiState(
+                    isError = true,
+                    errorMessage = "Failed to fetch details: ${e.message}"
+                )
             }
         }
     }
 }
-
+data class DetailTransaksiUiState(
+    val detailTransaksiUiEvent: InsertTransaksiUiEvent = InsertTransaksiUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = "",
+) {
+    val isUiEventNotEmpty: Boolean
+        get() = detailTransaksiUiEvent != InsertTransaksiUiEvent()
+}
 //memindahkan data dari entity ke ui
 fun Transaksi.toDetailTransaksiUiEvent(): InsertTransaksiUiEvent {
     return InsertTransaksiUiEvent(
         id_transaksi = id_transaksi,
-       id_tiket = id_tiket,
+        id_tiket = id_tiket,
         jumlah_tiket = jumlah_tiket,
-        jumlah_pembayaran = jumlah_pembayaran,
-        tanggal_transaksi = tanggal_transaksi
+        jumlah_pembayaran = jumlah_pembayaran
     )
 }
