62c6e3a597bb166c0a182d34f850867b1b8fbc85
dinaamlll <dinamaliaa17@gmail.com>
2025-01-28 20:36:25 +0700

    Menambahkan state `updateEventUiState` dan inisialisasi `_id_event` dari `SavedStateHandle`.

--- app/src/main/java/com/example/a13_179/ui/viewmodel/event/UpdateEventViewModel.kt
+++ app/src/main/java/com/example/a13_179/ui/viewmodel/event/UpdateEventViewModel.kt
@@ -1,25 +1,33 @@
 package com.example.a13_179.ui.viewmodel.event
 
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.a13_179.model.Event
+import com.example.a13_179.model.Peserta
 import com.example.a13_179.repository.EventRepository
+import com.example.a13_179.repository.PesertaRepository
 import com.example.a13_179.ui.view.event.DestinasiUpdateEvent
+import com.example.a13_179.ui.view.peserta.DestinasiUpdatePeserta
+import com.example.a13_179.ui.viewmodel.peserta.InsertPesertaUiEvent
+import com.example.a13_179.ui.viewmodel.peserta.InsertPesertaUiState
+import com.example.a13_179.ui.viewmodel.peserta.toPesertaUiState
+import com.example.a13_179.ui.viewmodel.peserta.toPsrta
 import kotlinx.coroutines.launch
 
 class UpdateEventViewModel(
     savedStateHandle: SavedStateHandle,
-    private val evnt: EventRepository
+    private val repositoryEvent: EventRepository
 ) : ViewModel() {
 
-    // Retrieve the NIM from SavedStateHandle
-    val id_event: Int = checkNotNull(savedStateHandle[DestinasiUpdateEvent.ID_EVENT])
-
-    var EventuiState = mutableStateOf(InsertEventUiState())
+    var updateEventUiState by mutableStateOf(InsertEventUiState())
         private set
+
+    private val _id_event: Int =
+        checkNotNull(savedStateHandle[DestinasiUpdateEvent.id_event])
 
     init {
         ambilEvent()
